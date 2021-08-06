32e441ad7a0b496364f6390054cd584ba866e630
Andrew Jang <dhj9817@gmail.com>
2021-08-07 06:06:06 +0900

    Footer2 fix

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -50,7 +50,7 @@ const Footer2 = () => {
 
     return (
         <div className="flex justify-start limit:justify-center w-screen bg-gray-f9f9f9 h-114px">
-            <div className="flex flex-col w-default flex-shrink-0 items-center justify-center space-y-10px">
+            <div className="flex flex-col w-default flex-shrink-0 items-center justify-center space-y-10px pl-10px">
                 <div className="flex justify-between w-default"><LogosList items={logosArray1} /></div>
                 <div className="flex justify-between w-default"><LogosList items={logosArray2} /></div>
             </div>
