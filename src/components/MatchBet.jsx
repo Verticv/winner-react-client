2a3b34707a4f5dbecdb6f35659f8c526c451d864
SKaralius <k.simonas.k@gmail.com>
2021-08-22 14:26:38 +0300

    Collapse or show bet.

--- src/components/MatchBet.jsx
+++ src/components/MatchBet.jsx
@@ -1,5 +1,5 @@
 import jss from "jss";
-import React from "react";
+import React, { useState } from "react";
 
 export default function MatchDetail({ bet }) {
     const styles = {
@@ -46,7 +46,14 @@ export default function MatchDetail({ bet }) {
                 text-align: right;
                 text-transform: uppercase;
             `,
+        rotate180: `
+                transform: rotate(180deg);
+        `
     };
+    const [isShowing, setIsShowing] = useState(true)
+    const handleToggleIsShowing = () => {
+        setIsShowing(prev => !prev)
+    }
     const { classes } = jss.createStyleSheet(styles).attach();
     const { betType, option1, option2, kof1, kof2, tieKof } = bet;
     return (
@@ -54,8 +61,16 @@ export default function MatchDetail({ bet }) {
             <div className={classes.betTitle}>
                 <div className="dot"></div>
                 <p className="text-115">{betType}</p>
-                <div className={classes.expandButton}>
-                    <div className="bg-holder-56">
+                <button
+                    onClick={handleToggleIsShowing}
+                    className={classes.expandButton}
+                >
+                    <div
+                        className={
+                            "bg-holder-56 " +
+                            `${isShowing ? "" : classes.rotate180}`
+                        }
+                    >
                         <img
                             className="ico-28"
                             src={require("../imagesHold/ico_21.png").default}
@@ -64,78 +79,88 @@ export default function MatchDetail({ bet }) {
                             height="10"
                         />
                     </div>
-                </div>
+                </button>
             </div>
-            <div className={classes.betOptions}>
-                <div className="group-56">
-                    <div className="bg-holder-57">
-                        <div className="row-5">
-                            <p className="text-116">{option1}</p>
-                            <p className="text-117">{kof1}</p>
+            {isShowing ? (
+                <div className={classes.betOptions}>
+                    <div className="group-56">
+                        <div className="bg-holder-57">
+                            <div className="row-5">
+                                <p className="text-116">{option1}</p>
+                                <p className="text-117">{kof1}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="bg-holder-58">
-                        <div className="bg-holder-59">
-                            <p className="text-118">{tieKof}</p>
+                        <div className="bg-holder-58">
+                            <div className="bg-holder-59">
+                                <p className="text-118">{tieKof}</p>
+                            </div>
+                            <img
+                                className="line-33"
+                                src={
+                                    require("../imagesHold/line_11.png").default
+                                }
+                                alt=""
+                                width="11"
+                                height="35"
+                            />
                         </div>
-                        <img
-                            className="line-33"
-                            src={require("../imagesHold/line_11.png").default}
-                            alt=""
-                            width="11"
-                            height="35"
-                        />
-                    </div>
-                    <div className={classes.team2}>
-                        <div className="row-6">
-                            <p className="text-119">{kof2}</p>
-                            <p className={classes.option2Name}>{option2}</p>
+                        <div className={classes.team2}>
+                            <div className="row-6">
+                                <p className="text-119">{kof2}</p>
+                                <p className={classes.option2Name}>{option2}</p>
+                            </div>
+                            <img
+                                className="line-34"
+                                src={
+                                    require("../imagesHold/line_12.png").default
+                                }
+                                alt=""
+                                width="11"
+                                height="35"
+                            />
                         </div>
-                        <img
-                            className="line-34"
-                            src={require("../imagesHold/line_12.png").default}
-                            alt=""
-                            width="11"
-                            height="35"
-                        />
+                        <div className="line-35"></div>
                     </div>
-                    <div className="line-35"></div>
+                    <div className="group-57">
+                        <div className="bg-holder-61">
+                            <div className="row-6">
+                                <p className="text-121">{option1}</p>
+                                <p className="text-122">{kof1}</p>
+                            </div>
+                        </div>
+                        <div className="bg-holder-62">
+                            <div className="bg-holder-63">
+                                <p className="text-123">{tieKof}</p>
+                            </div>
+                            <img
+                                className="line-36"
+                                src={
+                                    require("../imagesHold/line_11.png").default
+                                }
+                                alt=""
+                                width="11"
+                                height="35"
+                            />
+                        </div>
+                        <div className={classes.team2}>
+                            <div className="row-6">
+                                <p className="text-124">{kof2}</p>
+                                <p className={classes.option2Name}>{option2}</p>
+                            </div>
+                            <img
+                                className="line-37"
+                                src={
+                                    require("../imagesHold/line_12.png").default
+                                }
+                                alt=""
+                                width="11"
+                                height="35"
+                            />
+                        </div>
+                        <div className="line-38"></div>
+                    </div>
                 </div>
-                <div className="group-57">
-                    <div className="bg-holder-61">
-                        <div className="row-6">
-                            <p className="text-121">{option1}</p>
-                            <p className="text-122">{kof1}</p>
-                        </div>
-                    </div>
-                    <div className="bg-holder-62">
-                        <div className="bg-holder-63">
-                            <p className="text-123">{tieKof}</p>
-                        </div>
-                        <img
-                            className="line-36"
-                            src={require("../imagesHold/line_11.png").default}
-                            alt=""
-                            width="11"
-                            height="35"
-                        />
-                    </div>
-                    <div className={classes.team2}>
-                        <div className="row-6">
-                            <p className="text-124">{kof2}</p>
-                            <p className={classes.option2Name}>{option2}</p>
-                        </div>
-                        <img
-                            className="line-37"
-                            src={require("../imagesHold/line_12.png").default}
-                            alt=""
-                            width="11"
-                            height="35"
-                        />
-                    </div>
-                    <div className="line-38"></div>
-                </div>
-            </div>
+            ) : null}
         </div>
     );
 }
