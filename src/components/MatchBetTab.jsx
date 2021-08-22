a6e8c2705b2530baa03ddf65242d908cd8b574b7
SKaralius <k.simonas.k@gmail.com>
2021-08-22 15:52:17 +0300

    style match bet tabs.

--- src/components/MatchBetTab.jsx
+++ src/components/MatchBetTab.jsx
@@ -13,12 +13,14 @@ export default function MatchBetTab({ tab, handleSelectTab }) {
                 : require("../imagesHold/bg_149.jpg").default
         });
         background-size: cover;
+        background-position: top;
         margin-left: 1px;
         z-index: 3;
         `,
         tabText: `
             left: 19px;
             ${tab.selected ? "opacity: 1.0;" : "opacity: 0.4;"}
+            ${tab.selected ? "color: black;" : "color: white;"}
             line-height: 40px;
             text-align: center;
             text-transform: uppercase;
