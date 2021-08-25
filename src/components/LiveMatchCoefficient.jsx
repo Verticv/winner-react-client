e886a5bc6ed8f48cabd477c541a3c9a64bdb58cf
SKaralius <k.simonas.k@gmail.com>
2021-08-25 16:45:20 +0300

    Neon boxes for selections.

--- src/components/LiveMatchCoefficient.jsx
+++ src/components/LiveMatchCoefficient.jsx
@@ -1,3 +1,4 @@
+import jss from "jss";
 import React, { useState } from "react";
 
 export default function LiveMatchCoefficient({
@@ -7,6 +8,76 @@ export default function LiveMatchCoefficient({
     team2,
     team2WinKof,
 }) {
+     const styles = {
+         redNeonBorder: `
+            height: 32px;
+            left: 2px;
+            position: absolute;
+            width: 45px;
+            border: 1px solid #ffbc93; /*stroke*/
+            -webkit-border-radius: 3.88px/3.81px;
+            -moz-border-radius: 3.88px/3.81px;
+            border-radius: 3.88px/3.81px;
+            background: #7c2323;
+            -moz-background-clip: padding;
+            -webkit-background-clip: padding-box;
+            background-clip: padding-box; /*Will not allow bg color to leak outside borders*/
+            -webkit-box-shadow: 0 0 10.92px 1.08px #ff4c23;
+            -moz-box-shadow: 0 0 10.92px 1.08px #ff4c23;
+            box-shadow: 0 0 10.92px 1.08px #ff4c23; /*outer glow*/
+    `,
+         blueNeonBorder: `
+                height: 32px;
+                left: 0;
+                position: absolute;
+                top: 0;
+                width: 45px;
+                border: 1px solid #93d0ff; /*stroke*/
+                -webkit-border-radius: 3.88px/3.81px;
+                -moz-border-radius: 3.88px/3.81px;
+                border-radius: 3.88px/3.81px;
+                background: #293868;
+                -moz-background-clip: padding;
+                -webkit-background-clip: padding-box;
+                background-clip: padding-box; /*Will not allow bg color to leak outside borders*/
+                -webkit-box-shadow: 0 0 10.92px 1.08px #236bff;
+                -moz-box-shadow: 0 0 10.92px 1.08px #236bff;
+                box-shadow: 0 0 10.92px 1.08px #236bff; /*outer glow*/
+         `,
+         blueNeonBorderContainer: `
+         height: 32px;
+         left: 11px;
+         position: absolute;
+         top: 0;
+         width: 45px;
+         `,
+         greenNeonBorderContainer: `
+         height: 32px;
+         left: 23px;
+         position: absolute;
+         top: 0;
+         width: 45px;
+         `,
+         greenNeonBorder: `
+                height: 32px;
+                left: 0;
+                position: absolute;
+                top: 0;
+                width: 45px;
+                border: 1px solid #4ef11a; /*stroke*/
+                -webkit-border-radius: 3.88px/3.81px;
+                -moz-border-radius: 3.88px/3.81px;
+                border-radius: 3.88px/3.81px;
+                background: #246210;
+                -moz-background-clip: padding;
+                -webkit-background-clip: padding-box;
+                background-clip: padding-box; /*Will not allow bg color to leak outside borders*/
+                -webkit-box-shadow: 0 0 10.92px 1.08px #48cb1e;
+                -moz-box-shadow: 0 0 10.92px 1.08px #48cb1e;
+                box-shadow: 0 0 10.92px 1.08px #48cb1e; /*outer glow*/
+         `,
+     };
+    const { classes } = jss.createStyleSheet(styles).attach();
     const selections = { team1: 3, team2: 1, draw: 2, none: 0 };
     const [selectedOutcome, setSelectedOutcome] = useState(selections.none);
     function handleSetSelectedOutcome(outcome) {
@@ -29,6 +100,15 @@ export default function LiveMatchCoefficient({
                 }}
                 className="row-17"
             >
+                <div className="up">
+                    <div
+                        className={
+                            selectedOutcome === selections.team1
+                                ? classes.redNeonBorder
+                                : ""
+                        }
+                    ></div>
+                </div>
                 <p className="text-59">{team1}</p>
                 <p className="text-60">{team1WinKof}</p>
             </button>
@@ -37,7 +117,7 @@ export default function LiveMatchCoefficient({
                     style={
                         selectedOutcome === selections.draw
                             ? {
-                                //   background: "rgb(91,30,30)",
+                                  //   background: "rgb(91,30,30)",
                                   background:
                                       "radial-gradient(circle, rgba(91,30,30,1) 0%, rgba(155,47,48,1) 94%)",
                                   zIndex: 0,
@@ -57,6 +137,15 @@ export default function LiveMatchCoefficient({
                         width="11"
                         height="32"
                     />
+                    <div className={classes.greenNeonBorderContainer}>
+                        <div
+                            className={
+                                selectedOutcome === selections.draw
+                                    ? classes.greenNeonBorder
+                                    : ""
+                            }
+                        ></div>
+                    </div>
                     <p className="text-61">{tieKof}</p>
                 </button>
                 <button
@@ -73,6 +162,15 @@ export default function LiveMatchCoefficient({
                     }}
                     className="row-19"
                 >
+                    <div className={classes.blueNeonBorderContainer}>
+                        <div
+                            className={
+                                selectedOutcome === selections.team2
+                                    ? classes.blueNeonBorder
+                                    : ""
+                            }
+                        ></div>
+                    </div>
                     <p className="text-62">{team2WinKof}</p>
                     <p className="text-63">{team2}</p>
                 </button>
