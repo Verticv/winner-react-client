a03a1d2d66931716ef20dededebeabab9811a820
SKaralius <k.simonas.k@gmail.com>
2021-08-29 13:25:49 +0300

    Updating coefficients for match bets.

--- src/components/MatchBet.jsx
+++ src/components/MatchBet.jsx
@@ -1,5 +1,6 @@
 import jss from "jss";
 import React, { useState } from "react";
+import CoefficientWithUpdate from "./CoefficientWithUpdate";
 
 export default function MatchDetail({ bet }) {
     const styles = {
@@ -45,73 +46,6 @@ export default function MatchDetail({ bet }) {
         rotate180: `
                 transform: rotate(180deg);
         `,
-        redNeonBorder: `
-            height: 32px;
-            left: 2px;
-            position: absolute;
-            width: 45px;
-            border: 1px solid #ffbc93; /*stroke*/
-            -webkit-border-radius: 3.88px/3.81px;
-            -moz-border-radius: 3.88px/3.81px;
-            border-radius: 3.88px/3.81px;
-            background: #7c2323;
-            -moz-background-clip: padding;
-            -webkit-background-clip: padding-box;
-            background-clip: padding-box; /*Will not allow bg color to leak outside borders*/
-            -webkit-box-shadow: 0 0 10.92px 1.08px #ff4c23;
-            -moz-box-shadow: 0 0 10.92px 1.08px #ff4c23;
-            box-shadow: 0 0 10.92px 1.08px #ff4c23; /*outer glow*/
-    `,
-        blueNeonBorder: `
-                height: 32px;
-                left: 0;
-                position: absolute;
-                top: 0;
-                width: 45px;
-                border: 1px solid #93d0ff; /*stroke*/
-                -webkit-border-radius: 3.88px/3.81px;
-                -moz-border-radius: 3.88px/3.81px;
-                border-radius: 3.88px/3.81px;
-                background: #293868;
-                -moz-background-clip: padding;
-                -webkit-background-clip: padding-box;
-                background-clip: padding-box; /*Will not allow bg color to leak outside borders*/
-                -webkit-box-shadow: 0 0 10.92px 1.08px #236bff;
-                -moz-box-shadow: 0 0 10.92px 1.08px #236bff;
-                box-shadow: 0 0 10.92px 1.08px #236bff; /*outer glow*/
-                    `,
-        blueNeonBorderContainer: `
-                    height: 32px;
-                    left: 11px;
-                    position: absolute;
-                    top: 0;
-                    width: 45px;
-                    `,
-        greenNeonBorderContainer: `
-                    height: 32px;
-                    left: 23px;
-                    position: absolute;
-                    top: 0;
-                    width: 45px;
-                    `,
-        greenNeonBorder: `
-                            height: 32px;
-                            left: 0;
-                            position: absolute;
-                            top: 0;
-                            width: 45px;
-                            border: 1px solid #4ef11a; /*stroke*/
-                            -webkit-border-radius: 3.88px/3.81px;
-                            -moz-border-radius: 3.88px/3.81px;
-                            border-radius: 3.88px/3.81px;
-                            background: #246210;
-                            -moz-background-clip: padding;
-                            -webkit-background-clip: padding-box;
-                            background-clip: padding-box; /*Will not allow bg color to leak outside borders*/
-                            -webkit-box-shadow: 0 0 10.92px 1.08px #48cb1e;
-                            -moz-box-shadow: 0 0 10.92px 1.08px #48cb1e;
-                            box-shadow: 0 0 10.92px 1.08px #48cb1e; /*outer glow*/
-                    `,
     };
     const [isShowing, setIsShowing] = useState(true);
     const handleToggleIsShowing = () => {
@@ -170,19 +104,25 @@ export default function MatchDetail({ bet }) {
                                           })`,
                                           backgroundRepeat: "no-repeat",
                                           backgroundSize: "cover",
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          padding: '0 10px'
                                       }
                                     : {
                                           background: `url(${
                                               require("../imagesHold/bg_160.png")
                                                   .default
                                           })`,
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          padding: '0 10px'
                                       }
                             }
                         >
-                            <div className="row-5">
-                                <p className="text-116">{option1}</p>
-                                <p className="text-117">{kof1}</p>
-                            </div>
+                            <p className="text-116">{option1}</p>
+                            <CoefficientWithUpdate />
                         </button>
                         <button
                             onClick={() =>
@@ -198,18 +138,22 @@ export default function MatchDetail({ bet }) {
                                           })`,
                                           backgroundRepeat: "no-repeat",
                                           backgroundSize: "cover",
+                                          display: "flex",
+                                          justifyContent: "center",
+                                          alignItems: "center",
                                       }
                                     : {
                                           background: `url(${
                                               require("../imagesHold/bg_154.png")
                                                   .default
                                           })`,
+                                          display: "flex",
+                                          justifyContent: "center",
+                                          alignItems: "center",
                                       }
                             }
                         >
-                            <div className="bg-holder-59">
-                                <p className="text-118">{tieKof}</p>
-                            </div>
+                            <CoefficientWithUpdate />
                             <img
                                 className="line-33"
                                 src={
@@ -234,19 +178,25 @@ export default function MatchDetail({ bet }) {
                                           })`,
                                           backgroundRepeat: "no-repeat",
                                           backgroundSize: "cover",
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          paddingLeft: "10px",
                                       }
                                     : {
                                           background: `url(${
                                               require("../imagesHold/bg_156.png")
                                                   .default
                                           })`,
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          paddingLeft: "10px",
                                       }
                             }
                         >
-                            <div className="row-6">
-                                <p className="text-119">{kof2}</p>
-                                <p className={classes.option2Name}>{option2}</p>
-                            </div>
+                            <CoefficientWithUpdate />
+                            <p className={classes.option2Name}>{option2}</p>
                             <img
                                 className="line-34"
                                 src={
@@ -273,20 +223,26 @@ export default function MatchDetail({ bet }) {
                                           })`,
                                           backgroundRepeat: "no-repeat",
                                           backgroundSize: "cover",
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          padding: "0 10px",
                                       }
                                     : {
                                           background: `url(${
                                               require("../imagesHold/bg_160.png")
                                                   .default
                                           })`,
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          padding: "0 10px",
                                       }
                             }
                             className="bg-holder-61"
                         >
-                            <div className="row-6">
-                                <p className="text-121">{option1}</p>
-                                <p className="text-122">{kof1}</p>
-                            </div>
+                            <p className="text-121">{option1}</p>
+                            <CoefficientWithUpdate />
                         </button>
                         <button
                             onClick={() =>
@@ -301,19 +257,23 @@ export default function MatchDetail({ bet }) {
                                           })`,
                                           backgroundRepeat: "no-repeat",
                                           backgroundSize: "cover",
+                                          display: "flex",
+                                          justifyContent: "center",
+                                          alignItems: "center",
                                       }
                                     : {
                                           background: `url(${
                                               require("../imagesHold/bg_154.png")
                                                   .default
                                           })`,
+                                          display: "flex",
+                                          justifyContent: "center",
+                                          alignItems: "center",
                                       }
                             }
                             className="bg-holder-62"
                         >
-                            <div className="bg-holder-63">
-                                <p className="text-123">{tieKof}</p>
-                            </div>
+                            <CoefficientWithUpdate />
                             <img
                                 className="line-36"
                                 src={
@@ -338,19 +298,25 @@ export default function MatchDetail({ bet }) {
                                           })`,
                                           backgroundRepeat: "no-repeat",
                                           backgroundSize: "cover",
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          paddingLeft: "12px",
                                       }
                                     : {
                                           background: `url(${
                                               require("../imagesHold/bg_156.png")
                                                   .default
                                           })`,
+                                          display: "flex",
+                                          justifyContent: "space-between",
+                                          alignItems: "center",
+                                          paddingLeft: "12px",
                                       }
                             }
                         >
-                            <div className="row-6">
-                                <p className="text-124">{kof2}</p>
-                                <p className={classes.option2Name}>{option2}</p>
-                            </div>
+                            <CoefficientWithUpdate />
+                            <p className={classes.option2Name}>{option2}</p>
                             <img
                                 className="line-37"
                                 src={
