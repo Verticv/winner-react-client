739417784669d800acb7f8966ce60b2c696bc6b0
SKaralius <k.simonas.k@gmail.com>
2021-09-01 20:46:43 +0300

    Properly style lay out buttons.

--- src/components/SelectLayoutButton.jsx
+++ src/components/SelectLayoutButton.jsx
@@ -1,6 +1,6 @@
 import jss from "jss";
 import React from "react";
-const bg = require("../imagesHold/bg_135.png").default;
+const bg = require("../imagesHold/layout_button_background.png").default;
 const activeBg = require("../imagesHold/bg_134.png").default;
 export default function SelectLayoutButton({
     text,
@@ -19,33 +19,7 @@ export default function SelectLayoutButton({
                 -webkit-border-radius: 10px;
                 -moz-border-radius: 10px;
                 border-radius: 10px;
-                background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxIDEiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPgo8bGluZWFyR3JhZGllbnQgaWQ9ImJnIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjUwJSIgeTE9IjEwMCUiIHgyPSI1MCUiIHkyPSIwJSI+CjxzdG9wIG9mZnNldD0iMCIgc3RvcC1jb2xvcj0iIzAwMDAwMCIgc3RvcC1vcGFjaXR5PSIwLjI4IiAvPgo8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNmZmZmZmYiIHN0b3Atb3BhY2l0eT0iMC40IiAvPgo8L2xpbmVhckdyYWRpZW50Pgo8cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMSIgaGVpZ2h0PSIxIiBmaWxsPSJ1cmwoI2JnKSIgLz48L3N2Zz4=), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -moz-linear-gradient(90deg, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -o-linear-gradient(90deg, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -webkit-linear-gradient(90deg, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -webkit-gradient(linear, left bottom, left top, color-stop(0, rgba(0, 0, 0, 0.28)), to(rgba(255, 255, 255, 0.4))), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -webkit-linear-gradient(bottom, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -moz-linear-gradient(bottom, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: -o-linear-gradient(bottom, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
-                background: linear-gradient(0deg, rgba(0, 0, 0, 0.28) 0, rgba(255, 255, 255, 0.4) 100%), url(${
-                    active ? activeBg : bg
-                }) no-repeat;
+                background: url(${active ? activeBg : bg}) no-repeat;
                 -webkit-box-shadow: 0 3px 0 0 #351a27;
                 -moz-box-shadow: 0 3px 0 0 #351a27;
                 box-shadow: 0 3px 0 0 #351a27; /*drop shadow*/
@@ -54,15 +28,40 @@ export default function SelectLayoutButton({
     const { classes } = jss.createStyleSheet(styles).attach();
     return (
         <button className={classes.background} onClick={handleSetLayoutActive}>
-            <div className="group-24">
+            <div
+                style={{
+                    height: "38px",
+                    left: "8px",
+                    position: "absolute",
+                    top: "19px",
+                    width: "48px",
+                }}
+            >
                 <img
-                    className="ico-11"
+                    style={{
+                        position: "absolute",
+                        right: "16px",
+                        top: "0",
+                        filter: active ? "brightness(0%)" : "none",
+                    }}
                     src={image}
                     alt=""
                     width="16"
                     height="16"
                 />
-                <p className="text-22">{text}</p>
+                <p
+                    style={{
+                        left: "4px",
+                        position: "absolute",
+                        top: "23px",
+                        opacity: active ? "1" : "0.4",
+                        color: active ? "#000000" : "#fefefe",
+                        textAlign: "center",
+                        textTransform: "uppercase",
+                    }}
+                >
+                    {text}
+                </p>
             </div>
         </button>
     );
