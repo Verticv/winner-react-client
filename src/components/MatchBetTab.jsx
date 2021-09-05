5cf52914dddd702474c4ecfd1b5a3503f3f92c02
SKaralius <k.simonas.k@gmail.com>
2021-09-05 11:40:54 +0300

    MatchBetTabs selected on hover. New style.

--- src/components/MatchBetTab.jsx
+++ src/components/MatchBetTab.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import jss from "jss";
 import React from "react";
 
@@ -7,22 +8,44 @@ export default function MatchBetTab({ tab, handleSelectTab }) {
         padding: 0 16px;
         height: 40px;
         min-width: 66px;
+        box-sizing: border-box;
         background: url(${
             tab.selected
                 ? require("../imagesHold/bg_150.jpg").default
                 : require("../imagesHold/match_bet_background.png").default
         });
-        background-size: cover;
-        background-position: top;
-        margin-left: 2px;
+        background-size: 100% 100%;
+        margin-left: 1px;
         z-index: 3;
-        border-left: 1px solid;
-        border-right: 1px solid;
-        border-bottom: none;
-        border-top: 1px solid;
-        border-image-slice: 1;
-        border-image-source: linear-gradient(270deg, rgba(255,255,240,0.1) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 90%, rgba(255,255,240,0.1) 100%);
+        box-shadow: 0px 1px 1px 1px rgba(255,255,255,0.05) inset;
+        // border-left: 1px solid;
+        // border-right: 1px solid;
+        // border-bottom: none;
+        // border-top: 1px solid;
+        // border-image-slice: 1;
+        // border-image-source: linear-gradient(270deg, rgba(255,255,240,0.1) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 90%, rgba(255,255,240,0.1) 100%);
+         
+        ${
+            tab.selected
+                ? `
+        background-repeat: no-repeat;
+        border-left: none;
+        border-right: none;      
+        `
+                : ""
+        }
         `,
+        tabHover: {
+            "&:hover": {
+                background: `url(${
+                    require("../imagesHold/bg_150.jpg").default
+                })`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%",
+                borderLeft: "none",
+                borderRight: "none",
+            },
+        },
         tabText: `
             left: 19px;
             ${tab.selected ? "opacity: 1.0;" : "opacity: 0.4;"}
@@ -35,7 +58,10 @@ export default function MatchBetTab({ tab, handleSelectTab }) {
     const { classes } = jss.createStyleSheet(styles).attach();
 
     return (
-        <button onClick={handleSelectTab} className={classes.tab}>
+        <button
+            onClick={handleSelectTab}
+            className={clsx(classes.tab, classes.tabHover)}
+        >
             <p className={classes.tabText}>{tab.name}</p>
         </button>
     );
