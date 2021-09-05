d7e58f8d136d831fe4d5d3e9a1b6e48a3c3cd5ca
SKaralius <k.simonas.k@gmail.com>
2021-09-05 11:04:01 +0300

    Change the look of MatchBetTabs according to design.

--- src/components/MatchBetTab.jsx
+++ src/components/MatchBetTab.jsx
@@ -10,12 +10,18 @@ export default function MatchBetTab({ tab, handleSelectTab }) {
         background: url(${
             tab.selected
                 ? require("../imagesHold/bg_150.jpg").default
-                : require("../imagesHold/bg_149.jpg").default
+                : require("../imagesHold/match_bet_background.png").default
         });
         background-size: cover;
         background-position: top;
-        margin-left: 1px;
+        margin-left: 2px;
         z-index: 3;
+        border-left: 1px solid;
+        border-right: 1px solid;
+        border-bottom: none;
+        border-top: 1px solid;
+        border-image-slice: 1;
+        border-image-source: linear-gradient(270deg, rgba(255,255,240,0.1) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 90%, rgba(255,255,240,0.1) 100%);
         `,
         tabText: `
             left: 19px;
